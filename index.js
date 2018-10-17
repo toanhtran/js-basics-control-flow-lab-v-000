33cb6d423cf1abdc354f2441a66a2bb45259d253
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-17 20:44:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(feet) {
-
+function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
     return 'This one is on me!';
-  } else if (1999 < feet && feet  < 2500 ) {
-    return 'I will gladly take your thirty bucks.';
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
   } else {
-    return 'No can do.';
+    return "No can do.";
   }
 }
 
